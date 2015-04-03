1fa05aead8790f26a0c8a324fcc8053cee3b1e63
Joker <fabio.favero@gmail.com>
2015-04-03 09:52:32 -0300

    Initial commit.

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "coursera-scala-one"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+    
---
+++ src/main/scala/ffhenkes/coursera/scala/one/One.scala
@@ -0,0 +1,8 @@
+package ffhenkes.coursera.scala.one
+
+/**
+ * Created by fabio on 03/04/15.
+ */
+object One extends App {
+  println("Hello Scala World!");
+}
