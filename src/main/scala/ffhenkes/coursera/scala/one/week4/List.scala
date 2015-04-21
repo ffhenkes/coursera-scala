7f68702030956e0c9fb9b071d20917952de7eab3
Joker <fabio.favero@gmail.com>
2015-04-21 15:02:44 -0300

    [ADD] Polymorphism

---
+++ src/main/scala/ffhenkes/coursera/scala/one/week4/List.scala
@@ -0,0 +1,20 @@
+package ffhenkes.coursera.scala.one.week4
+
+/**
+ * Created by fabio on 21/04/15.
+ */
+trait List[T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: List[T]
+}
+
+class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  def isEmpty: Boolean = false
+}
+
+class Nil[T] extends List[T] {
+  def isEmpty: Boolean = true
+  def head: Nothing = throw new NoSuchElementException("Nil.head")
+  def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
